7a95ae274d77eda1471908c1c12009c0e54ab848
Tom Cox <tom.jcox@icloud.com>
2014-12-11 18:03:18 +0000

    Update seeds.rb
    Added temp json seed. Might not work.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,616 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Temp JSON Seed
+
+{
+	"cols": [
+		"title",
+		"description",
+		"price",
+		"year_published"
+	],
+	"data": [
+		[
+			"porttitor",
+			"Suspendisse sagittis. Nullam vitae diam. Proin dolor. Nulla semper tellus",
+			"50.82",
+			2009
+		],
+		[
+			"Aenean",
+			"lorem, eget mollis lectus pede et risus. Quisque libero lacus,",
+			"78.56",
+			2001
+		],
+		[
+			"vel",
+			"urna. Nunc quis arcu vel quam dignissim pharetra. Nam ac",
+			"74.31",
+			2005
+		],
+		[
+			"mi",
+			"urna justo faucibus lectus, a sollicitudin orci sem eget massa.",
+			"72.73",
+			2010
+		],
+		[
+			"lobortis",
+			"a ultricies adipiscing, enim mi tempor lorem, eget mollis lectus",
+			"68.09",
+			2005
+		],
+		[
+			"sagittis.",
+			"arcu. Sed eu nibh vulputate mauris sagittis placerat. Cras dictum",
+			"53.49",
+			2011
+		],
+		[
+			"ante",
+			"et magnis dis parturient montes, nascetur ridiculus mus. Proin vel",
+			"33.35",
+			2001
+		],
+		[
+			"dui,",
+			"Proin ultrices. Duis volutpat nunc sit amet metus. Aliquam erat",
+			"67.53",
+			2013
+		],
+		[
+			"malesuada",
+			"eu eros. Nam consequat dolor vitae dolor. Donec fringilla. Donec",
+			"63.52",
+			2002
+		],
+		[
+			"a,",
+			"enim. Mauris quis turpis vitae purus gravida sagittis. Duis gravida.",
+			"91.88",
+			2005
+		],
+		[
+			"id,",
+			"diam at pretium aliquet, metus urna convallis erat, eget tincidunt",
+			"54.60",
+			2010
+		],
+		[
+			"nec",
+			"Vestibulum ante ipsum primis in faucibus orci luctus et ultrices",
+			"6.39",
+			2009
+		],
+		[
+			"felis",
+			"Sed molestie. Sed id risus quis diam luctus lobortis. Class",
+			"61.11",
+			2007
+		],
+		[
+			"tristique",
+			"Fusce diam nunc, ullamcorper eu, euismod ac, fermentum vel, mauris.",
+			"73.74",
+			2002
+		],
+		[
+			"Maecenas",
+			"non, dapibus rutrum, justo. Praesent luctus. Curabitur egestas nunc sed",
+			"16.02",
+			2008
+		],
+		[
+			"vitae",
+			"lorem eu metus. In lorem. Donec elementum, lorem ut aliquam",
+			"59.41",
+			2001
+		],
+		[
+			"at,",
+			"dolor. Quisque tincidunt pede ac urna. Ut tincidunt vehicula risus.",
+			"42.47",
+			2009
+		],
+		[
+			"amet",
+			"Donec consectetuer mauris id sapien. Cras dolor dolor, tempus non,",
+			"36.04",
+			2009
+		],
+		[
+			"feugiat",
+			"lorem ipsum sodales purus, in molestie tortor nibh sit amet",
+			"16.93",
+			2005
+		],
+		[
+			"lobortis",
+			"at, egestas a, scelerisque sed, sapien. Nunc pulvinar arcu et",
+			"91.57",
+			2002
+		],
+		[
+			"pede,",
+			"dignissim lacus. Aliquam rutrum lorem ac risus. Morbi metus. Vivamus",
+			"44.13",
+			2007
+		],
+		[
+			"est.",
+			"eu tellus. Phasellus elit pede, malesuada vel, venenatis vel, faucibus",
+			"97.09",
+			2011
+		],
+		[
+			"eget,",
+			"at fringilla purus mauris a nunc. In at pede. Cras",
+			"53.67",
+			2010
+		],
+		[
+			"Nulla",
+			"tincidunt adipiscing. Mauris molestie pharetra nibh. Aliquam ornare, libero at",
+			"14.37",
+			2006
+		],
+		[
+			"mi",
+			"nisi nibh lacinia orci, consectetuer euismod est arcu ac orci.",
+			"62.11",
+			2004
+		],
+		[
+			"pellentesque,",
+			"at, velit. Pellentesque ultricies dignissim lacus. Aliquam rutrum lorem ac",
+			"13.80",
+			2004
+		],
+		[
+			"rhoncus",
+			"Etiam ligula tortor, dictum eu, placerat eget, venenatis a, magna.",
+			"84.81",
+			2007
+		],
+		[
+			"nonummy",
+			"dolor. Quisque tincidunt pede ac urna. Ut tincidunt vehicula risus.",
+			"63.77",
+			2004
+		],
+		[
+			"metus.",
+			"libero lacus, varius et, euismod et, commodo at, libero. Morbi",
+			"14.67",
+			2006
+		],
+		[
+			"tortor",
+			"eu tellus eu augue porttitor interdum. Sed auctor odio a",
+			"64.14",
+			2005
+		],
+		[
+			"Suspendisse",
+			"Etiam laoreet, libero et tristique pellentesque, tellus sem mollis dui,",
+			"0.83",
+			2006
+		],
+		[
+			"ante",
+			"at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada",
+			"35.63",
+			2008
+		],
+		[
+			"lectus.",
+			"urna suscipit nonummy. Fusce fermentum fermentum arcu. Vestibulum ante ipsum",
+			"4.50",
+			2013
+		],
+		[
+			"leo,",
+			"dolor, tempus non, lacinia at, iaculis quis, pede. Praesent eu",
+			"26.08",
+			2012
+		],
+		[
+			"dignissim",
+			"a odio semper cursus. Integer mollis. Integer tincidunt aliquam arcu.",
+			"10.47",
+			2002
+		],
+		[
+			"fames",
+			"In faucibus. Morbi vehicula. Pellentesque tincidunt tempus risus. Donec egestas.",
+			"8.00",
+			2013
+		],
+		[
+			"penatibus",
+			"neque tellus, imperdiet non, vestibulum nec, euismod in, dolor. Fusce",
+			"78.99",
+			2013
+		],
+		[
+			"In",
+			"Etiam vestibulum massa rutrum magna. Cras convallis convallis dolor. Quisque",
+			"58.06",
+			2013
+		],
+		[
+			"non",
+			"luctus aliquet odio. Etiam ligula tortor, dictum eu, placerat eget,",
+			"67.35",
+			2013
+		],
+		[
+			"tellus",
+			"Nulla facilisi. Sed neque. Sed eget lacus. Mauris non dui",
+			"8.64",
+			2002
+		],
+		[
+			"amet,",
+			"Fusce mollis. Duis sit amet diam eu dolor egestas rhoncus.",
+			"9.58",
+			2013
+		],
+		[
+			"amet,",
+			"nibh lacinia orci, consectetuer euismod est arcu ac orci. Ut",
+			"26.62",
+			2011
+		],
+		[
+			"justo.",
+			"in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus",
+			"37.37",
+			2008
+		],
+		[
+			"pede.",
+			"enim. Mauris quis turpis vitae purus gravida sagittis. Duis gravida.",
+			"58.73",
+			2005
+		],
+		[
+			"orci",
+			"non, dapibus rutrum, justo. Praesent luctus. Curabitur egestas nunc sed",
+			"15.59",
+			2008
+		],
+		[
+			"aliquam",
+			"ac mattis velit justo nec ante. Maecenas mi felis, adipiscing",
+			"65.11",
+			2013
+		],
+		[
+			"at,",
+			"magnis dis parturient montes, nascetur ridiculus mus. Aenean eget magna.",
+			"21.41",
+			2008
+		],
+		[
+			"enim,",
+			"faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus ornare.",
+			"46.96",
+			2012
+		],
+		[
+			"eget,",
+			"lorem lorem, luctus ut, pellentesque eget, dictum placerat, augue. Sed",
+			"14.34",
+			2006
+		],
+		[
+			"amet",
+			"semper rutrum. Fusce dolor quam, elementum at, egestas a, scelerisque",
+			"64.91",
+			2006
+		],
+		[
+			"tristique",
+			"lacus. Quisque imperdiet, erat nonummy ultricies ornare, elit elit fermentum",
+			"72.74",
+			2006
+		],
+		[
+			"luctus.",
+			"metus. In nec orci. Donec nibh. Quisque nonummy ipsum non",
+			"80.64",
+			2000
+		],
+		[
+			"mauris",
+			"Praesent eu dui. Cum sociis natoque penatibus et magnis dis",
+			"72.50",
+			2003
+		],
+		[
+			"aliquet",
+			"semper tellus id nunc interdum feugiat. Sed nec metus facilisis",
+			"9.61",
+			2001
+		],
+		[
+			"Cras",
+			"elit pede, malesuada vel, venenatis vel, faucibus id, libero. Donec",
+			"79.55",
+			2004
+		],
+		[
+			"luctus",
+			"aliquam arcu. Aliquam ultrices iaculis odio. Nam interdum enim non",
+			"44.75",
+			2002
+		],
+		[
+			"eget,",
+			"et, commodo at, libero. Morbi accumsan laoreet ipsum. Curabitur consequat,",
+			"54.51",
+			2010
+		],
+		[
+			"mauris.",
+			"ante dictum cursus. Nunc mauris elit, dictum eu, eleifend nec,",
+			"52.59",
+			2011
+		],
+		[
+			"aliquet",
+			"dapibus id, blandit at, nisi. Cum sociis natoque penatibus et",
+			"29.69",
+			2003
+		],
+		[
+			"felis",
+			"aliquam eu, accumsan sed, facilisis vitae, orci. Phasellus dapibus quam",
+			"69.37",
+			2014
+		],
+		[
+			"eu",
+			"non, egestas a, dui. Cras pellentesque. Sed dictum. Proin eget",
+			"36.99",
+			2008
+		],
+		[
+			"vitae",
+			"lobortis quis, pede. Suspendisse dui. Fusce diam nunc, ullamcorper eu,",
+			"84.06",
+			2002
+		],
+		[
+			"ligula",
+			"in magna. Phasellus dolor elit, pellentesque a, facilisis non, bibendum",
+			"83.93",
+			2005
+		],
+		[
+			"nunc",
+			"est, mollis non, cursus non, egestas a, dui. Cras pellentesque.",
+			"49.50",
+			2005
+		],
+		[
+			"metus",
+			"Curabitur egestas nunc sed libero. Proin sed turpis nec mauris",
+			"56.16",
+			2012
+		],
+		[
+			"posuere",
+			"semper, dui lectus rutrum urna, nec luctus felis purus ac",
+			"91.88",
+			2005
+		],
+		[
+			"feugiat",
+			"quam quis diam. Pellentesque habitant morbi tristique senectus et netus",
+			"6.25",
+			2009
+		],
+		[
+			"adipiscing",
+			"sem elit, pharetra ut, pharetra sed, hendrerit a, arcu. Sed",
+			"49.81",
+			2013
+		],
+		[
+			"non,",
+			"tellus. Phasellus elit pede, malesuada vel, venenatis vel, faucibus id,",
+			"18.91",
+			2005
+		],
+		[
+			"metus",
+			"bibendum fermentum metus. Aenean sed pede nec ante blandit viverra.",
+			"14.95",
+			2006
+		],
+		[
+			"mi",
+			"vulputate velit eu sem. Pellentesque ut ipsum ac mi eleifend",
+			"26.45",
+			2011
+		],
+		[
+			"feugiat",
+			"tristique ac, eleifend vitae, erat. Vivamus nisi. Mauris nulla. Integer",
+			"42.82",
+			2008
+		],
+		[
+			"velit",
+			"Proin nisl sem, consequat nec, mollis vitae, posuere at, velit.",
+			"23.84",
+			2004
+		],
+		[
+			"Sed",
+			"auctor quis, tristique ac, eleifend vitae, erat. Vivamus nisi. Mauris",
+			"27.68",
+			2002
+		],
+		[
+			"sit",
+			"Donec tempor, est ac mattis semper, dui lectus rutrum urna,",
+			"78.42",
+			2006
+		],
+		[
+			"sodales",
+			"massa. Suspendisse eleifend. Cras sed leo. Cras vehicula aliquet libero.",
+			"16.44",
+			2000
+		],
+		[
+			"sollicitudin",
+			"ornare tortor at risus. Nunc ac sem ut dolor dapibus",
+			"66.31",
+			2002
+		],
+		[
+			"urna.",
+			"Proin ultrices. Duis volutpat nunc sit amet metus. Aliquam erat",
+			"30.67",
+			2010
+		],
+		[
+			"a,",
+			"lectus. Cum sociis natoque penatibus et magnis dis parturient montes,",
+			"0.11",
+			2004
+		],
+		[
+			"Suspendisse",
+			"fringilla mi lacinia mattis. Integer eu lacus. Quisque imperdiet, erat",
+			"84.27",
+			2004
+		],
+		[
+			"venenatis",
+			"magna. Sed eu eros. Nam consequat dolor vitae dolor. Donec",
+			"65.03",
+			2014
+		],
+		[
+			"Nulla",
+			"scelerisque neque. Nullam nisl. Maecenas malesuada fringilla est. Mauris eu",
+			"20.31",
+			2002
+		],
+		[
+			"eget",
+			"eu tellus. Phasellus elit pede, malesuada vel, venenatis vel, faucibus",
+			"50.64",
+			2000
+		],
+		[
+			"at",
+			"enim diam vel arcu. Curabitur ut odio vel est tempor",
+			"25.04",
+			2008
+		],
+		[
+			"Nullam",
+			"urna, nec luctus felis purus ac tellus. Suspendisse sed dolor.",
+			"80.78",
+			2009
+		],
+		[
+			"egestas",
+			"amet, faucibus ut, nulla. Cras eu tellus eu augue porttitor",
+			"84.99",
+			2007
+		],
+		[
+			"Nunc",
+			"at, velit. Pellentesque ultricies dignissim lacus. Aliquam rutrum lorem ac",
+			"37.36",
+			2006
+		],
+		[
+			"Sed",
+			"erat vel pede blandit congue. In scelerisque scelerisque dui. Suspendisse",
+			"43.66",
+			2009
+		],
+		[
+			"risus,",
+			"Quisque ornare tortor at risus. Nunc ac sem ut dolor",
+			"37.13",
+			2012
+		],
+		[
+			"Aenean",
+			"tempor bibendum. Donec felis orci, adipiscing non, luctus sit amet,",
+			"82.29",
+			2006
+		],
+		[
+			"blandit.",
+			"augue ut lacus. Nulla tincidunt, neque vitae semper egestas, urna",
+			"10.21",
+			2012
+		],
+		[
+			"auctor.",
+			"nec ante blandit viverra. Donec tempus, lorem fringilla ornare placerat,",
+			"27.10",
+			2006
+		],
+		[
+			"urna.",
+			"vitae diam. Proin dolor. Nulla semper tellus id nunc interdum",
+			"52.32",
+			2004
+		],
+		[
+			"dis",
+			"molestie dapibus ligula. Aliquam erat volutpat. Nulla dignissim. Maecenas ornare",
+			"62.48",
+			2004
+		],
+		[
+			"posuere",
+			"pellentesque a, facilisis non, bibendum sed, est. Nunc laoreet lectus",
+			"96.54",
+			2005
+		],
+		[
+			"a,",
+			"erat, in consectetuer ipsum nunc id enim. Curabitur massa. Vestibulum",
+			"16.27",
+			2006
+		],
+		[
+			"turpis",
+			"lectus pede, ultrices a, auctor non, feugiat nec, diam. Duis",
+			"30.22",
+			2000
+		],
+		[
+			"per",
+			"tincidunt, nunc ac mattis ornare, lectus ante dictum mi, ac",
+			"32.53",
+			2000
+		],
+		[
+			"nec",
+			"diam. Pellentesque habitant morbi tristique senectus et netus et malesuada",
+			"57.61",
+			2004
+		],
+		[
+			"eget,",
+			"pede, malesuada vel, venenatis vel, faucibus id, libero. Donec consectetuer",
+			"4.93",
+			2000
+		]
+	]
+}
